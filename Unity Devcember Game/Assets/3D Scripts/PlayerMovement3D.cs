41058cdd74abc2deab00f6ee5aa059971fe15e98
Meir <meirrudenstein@gmail.com>
2022-12-06 13:20:54 +0200

    3D MOVEMENT IS NOW WORKINGgit add .

--- Unity Devcember Game/Assets/3D Scripts/PlayerMovement3D.cs
+++ Unity Devcember Game/Assets/3D Scripts/PlayerMovement3D.cs
@@ -7,7 +7,6 @@ public class PlayerMovement3D : MonoBehaviour
     public Transform cam;
     public Transform rot;
     public float speed = 4f;
-    public float testMod = 45f;
 
     private Rigidbody rb;
 
@@ -23,84 +22,32 @@ public class PlayerMovement3D : MonoBehaviour
         Vector3 cameraPosition = cam.position;
         Vector3 playerPosition = transform.position;
 
-        Vector3 tempCameraPosition = new Vector3(
-            cameraPosition.x,
-            cameraPosition.z,
-            0f);
+        float ValX = playerPosition.x - cameraPosition.x;
+        float ValY = playerPosition.z - cameraPosition.z;
 
-        Vector3 tempPlayerPosition = new Vector3(
-            playerPosition.x,
-            playerPosition.z,
-            0f);
-
-        float ValX = tempPlayerPosition.x - tempCameraPosition.x;
-        float ValY = tempPlayerPosition.y - tempCameraPosition.y;
-
-        if (ValX < 0) {
-            ValX 
-        }
-        else if (ValY < 0) {
-
-        }
-
-        float angle = Mathf.Atan(ValY / ValX) * Mathf.Rad2Deg;
-
-
-        
-
-        rb.velocity = new Vector3(0f, 0f, 0f);
-        Vector3 tempVelocity = rb.velocity;
+        float angle = Mathf.Atan2(ValX, ValY) * Mathf.Rad2Deg; //Returns the angle between -180 and 180.
+        angle += 90;
 
         rot.rotation = Quaternion.Euler(0f, angle, 0f);
         Debug.Log(angle);
 
 
+
+
+        rb.velocity = new Vector3(0f, 0f, 0f);
+        Vector3 tempVelocity = rb.velocity;
+
         if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) {
-            //Get camera and player position
-            //Vector3 cameraPosition = cam.position;
-            //Vector3 playerPosition = transform.position;
+            if (Input.GetAxis("Horizontal") != 0) {
+                tempVelocity += rot.transform.forward * Input.GetAxis("Horizontal");
+            }
 
-            ////get differrence for movement calculation
-            //float ValX = playerPosition.x - cameraPosition.x;
-            //float ValZ = playerPosition.z - cameraPosition.z;
+            if (Input.GetAxis("Vertical") != 0) {
+                tempVelocity += rot.transform.right * Input.GetAxis("Vertical") * -1;
+            }
 
-            //rb.velocity = new Vector3(0f, 0f, 0f);
-            //Vector3 tempVelocity = rb.velocity;
-            //float angle = Mathf.Atan(ValX / ValZ * Mathf.Deg2Rad);
-
-            //rot.rotation = Quaternion.Euler(0f, angle, 0f);
-            //Debug.Log(angle);
-
-            //if (Input.GetAxis("Vertical") > 0) {
-            //    tempVelocity.x += ValX;
-            //    tempVelocity.z += ValZ;
-            //} else if (Input.GetAxis("Vertical") < 0) {
-            //    tempVelocity.x += ValX * -1;
-            //    tempVelocity.z += ValZ * -1;
-            //}
-
-            //if (Input.GetAxis("Horizontal") != 0) {
-            //    float angle = Mathf.Atan(ValX / ValZ * Mathf.Deg2Rad);
-            //    if (Input.GetAxis("Horizontal") > 0)
-            //    {
-            //        tempVelocity.x += Mathf.Sin(angle) * Mathf.Rad2Deg;
-            //        tempVelocity.z += Mathf.Cos(angle) * Mathf.Rad2Deg;
-            //    }
-            //    else if (Input.GetAxis("Horizontal") < 0)
-            //    {
-            //        tempVelocity.x += Mathf.Sin(angle) * Mathf.Rad2Deg * -1;
-            //        tempVelocity.z += Mathf.Cos(angle) * Mathf.Rad2Deg * -1;
-            //    }
-            //}
-
-            //rb.velocity = tempVelocity.normalized * speed;
+            rb.velocity = tempVelocity.normalized * speed;
         }
-    }
-
-    public static float CalculateAngle(Vector3 from, Vector3 to)
-    {
-        return Quaternion.FromToRotation(Vector3.up, to - from).eulerAngles.z;
-
     }
 
 }
